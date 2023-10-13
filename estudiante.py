624f99e7f579c7d7edd6f2dffa97f229f09a76f6
Salvador Garcia <salvadorgarciaescribano@gmail.com>
2023-10-13 18:47:13 -0300

    Validacion_gmailandcontra_casi

--- estudiante.py
+++ estudiante.py
@@ -5,8 +5,8 @@ from curso import Curso
 lista_alum_registrado = []
 
 class Estudante(Usuario):
-    def __init__(self, legajo: int, anio_increcion_carrera: int):
-        
+    def __init__(self, legajo: int, anio_increcion_carrera: int, nombre: str, apellido: str, email: str, contrasena: str):
+        super().__init__(nombre, apellido, email, contrasena)
         self.__legajo = legajo
         self.__anio_increcion_carrera = anio_increcion_carrera
         
@@ -15,4 +15,19 @@ def __str__(self):
     
 def matricular_en_curso(self, curso : Curso):
     pass
+
+def validar_credenciales(self, email: str, contrasena: str):
+    for alumno in lista_alum_registrado:
+        if alumno.email == email and alumno.contrasena == contrasena:
+            print("Acceso concedido")
+        return
+    print("Correo electronico o contraseña incorrecta ")
+        
+        
     
+        
+estudante1 = Estudante(425323, 2022)
+lista_alum_registrado.append(estudante1)
+
+
+
--- usuario.py
+++ usuario.py
@@ -1,15 +1,23 @@
 from abc import ABC, abstractmethod
-from datetime import date
 
 class Usuario(ABC):
-    def __init__(self, nombre: str, apellido: str, email: str, contrasena: str):
-        self.__nombre = nombre
-        self.__apellido = apellido
-        self.__email = email
-        self.__contrasena = contrasena
-def __str__(self):
-    pass
-def validar_credenciales(self, email: str, contrasena: str) -> bool:
-    pass
+    conj_emails = set()
+    def init(self, nombre: str, apellido: str, email: str, contrasenia: str):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.contrasenia = contrasenia
+
+        if email in Usuario.conj_emails:
+            raise ValueError("El email ya existe, debe proporcionar otro")
+        else:
+            self.email = email
+            Usuario.conj_emails.add(email)
 
 
+    def str(self):
+        pass
+
+
+    def validar_credenciales(self, email: str, contrasenia: str) -> bool:
+        pass
