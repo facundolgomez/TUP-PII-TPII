cd1269a7932d879d554d62741d09fb5af8c1d6ca
Salvador Garcia <salvadorgarciaescribano@gmail.com>
2023-10-13 16:36:54 -0300

    Menu_app_completo

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 #TP2-Programacion2
 while True:
-    print("1-Ingresar como alumno")
-    print("2-Ingresar como profesor")
-    print("3-Ver cursos")
-    print("4-Salir")
+    print("Bienvenido al Campus Virtual")
+    print("<--------------------------->")
+    print("1 -> Ingresar como alumno")
+    print("2 -> Ingresar como profesor")
+    print("3 -> Ver cursos")
+    print("4 -> Salir")
 
     op = input("Seleccione una opción: ")
 
     if op == "1":
-        pass
+        print("Campus Virtual --> Alumno")
+        print("1 -> matricularse a un curso")
+        print("2 -> Ver cursos matriculados")
+        print("3 -> Volver al menu principal")
+        break
 
     elif op == "2":
-        pass 
+        print("Campus Virtual --> Prosefor")
+        print("1 -> Dictar Curso")
+        print("2 -> Ver curso")
+        print("3 -> Volver al menu principal")
+        break 
 
     elif op == "3":
-        pass
+        break
 
     elif op == "4":
 
         break
+    
+    
+    
