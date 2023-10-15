55e1fbd9251180ad0cf86611e2eb35ff36e0101e
facundolgomez <facundolgomez87@gmail.com>
2023-10-15 13:59:12 -0300

    creacion de un archivo llamado submenualumno

---
+++ submenualumno.py
@@ -0,0 +1,20 @@
+#archivo para hacer los submenu y ahorrar codigo
+
+def submenu():
+    print("1 -> Matricularse a un curso")
+    print("2 -> Ver curso")
+    print("3 -> Volver al menú principal")
+    op = input("Seleccione una opción: ")
+    return op
+
+
+
+
+def materias():
+    print("<-- Materias a matricularse -->")
+    print("1 -> Programación I")
+    print("2 -> Programación II")
+    print("3 -> Laboratorio II")
+    print("4 -> Inglés I")
+    print("5 -> Inglés II")
+
