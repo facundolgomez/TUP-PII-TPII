bb9a15b61318673b53c03c25f1b71b635edb4b36
facundolgomez <facundolgomez87@gmail.com>
2023-10-16 01:50:28 -0300

    agregando una lista como variable de instancia

--- profesor.py
+++ profesor.py
@@ -1,6 +1,7 @@
 from usuario import Usuario
 from curso import Curso
 
+
 lista_profes_registrados = []
 
 class Profesor(Usuario):
@@ -8,20 +9,33 @@ class Profesor(Usuario):
         super().__init__(nombre, apellido, email, contrasenia)
         self.__titulo = titulo
         self.__anio_egreso = anio_egreso
+        self.mis_cursos = []
 
     def dictar_curso(self, curso: Curso):
-        pass
+        self.mis_cursos.append(curso)
 
     def __str__(self):
-        return f"Profesor: {self.nombre} {self.apellido}, Email: {self.email}, Título: {self.__titulo}"
+        pass
 
     
     #<----------------Hecho por salvi XD nose si estara bien :D espero que si-------------------------->
     
-    def validar_credenciales(self, email, contrasena) -> bool:
+    def validar_credenciales(self, email, contrasenia_ingresada) -> bool:
+        acceso_concedido = False
         for profe in lista_profes_registrados:
-            if profe.email == email and profe.contrasena == contrasena:
-                print("Acceso concedido")
-            return
+            if profe.email == email and profe.contrasenia == contrasenia_ingresada:
+                acceso_concedido = True
+                print("ACCESO EXITOSO")
+                return acceso_concedido
         print("Correo electronico o contraseña incorrecta ")
         
+
+profe = Profesor("Carlos", "rodriguez", "mailprofe", "contra", "ingeniero", 1998)
+lista_profes_registrados.append(profe)
+
+#tener en cuenta que esta funcion es para buscar si existe el profesor, no pertenece a la clase
+def buscando_profesor(email):
+    for profe in lista_profes_registrados:
+        if profe.email == email:
+            return profe
+    return None
