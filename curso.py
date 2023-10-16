2ff7ef48789772459fc6acf277ff727c1462cde2
facundolgomez <facundolgomez87@gmail.com>
2023-10-16 01:45:17 -0300

    haciendo funcion de crear contraseña

--- curso.py
+++ curso.py
@@ -1,16 +1,20 @@
+import random
+import string
 
 class Curso:
-    def __init__(self, nombre: str, contrasenia_metriculacion: str):
+    def __init__(self, nombre: str):
         self.__nombre = nombre
-        self.__contrasenia_metriculacion = contrasenia_metriculacion
-    
+        self.__contrasenia_matriculacion = self.__generador_contrasenia()
+
     def __str__(self):
         pass
 
-    def __generador_contrasenia(self) -> str:
-        pass
 
- 
- 
- 
+    def __generador_contrasenia(self):
+        longitud = 6
+        caracteres = string.ascii_letters + string.digits
+        contrasenia = ''
+        for x in range(longitud):
+            contrasenia += random.choice(caracteres)
+        return contrasenia
 
