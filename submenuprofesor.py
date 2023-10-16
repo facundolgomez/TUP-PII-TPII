dc253c9c417a79042b4efd9409c24dd2de3bb94c
facundolgomez <facundolgomez87@gmail.com>
2023-10-16 16:34:26 -0300

    modificando sintaxis

--- submenuprofesor.py
+++ submenuprofesor.py
@@ -9,25 +9,22 @@ def submenu(profesor_actual):
         op = input("Seleccione una opción: ")
 
         if op == "1":
-            #dictar curso
+            # Dictar curso
             nombre_curso = input("Ingrese el nombre del curso a dar de alta: ")
             nuevo_curso = curso.Curso(nombre_curso)
             profesor_actual.dictar_curso(nuevo_curso)
-            print(f"Curso '{nuevo_curso.get_nombre()}' dado de alta con éxito.")
-            print(f"Nombre: {nuevo_curso.get_nombre()}")
-            print(f"Contraseña: {nuevo_curso.get_contrasenia_matriculacion()}")
+            print(f"Curso '{nuevo_curso.nombre}' dado de alta con éxito.")
+            print(f"Nombre: {nuevo_curso.nombre}")
+            print(f"Contraseña: {nuevo_curso.contrasenia_matriculacion}")
 
-
-
-        
         elif op == "2":
-        #ver cursos
+            # Ver cursos
             if not profesor_actual.mis_cursos:
                 print("El profesor no ha dictado cursos todavía.")
             else:
                 for num, curso_actual in enumerate(profesor_actual.mis_cursos):
-                    print(f"{num + 1} {curso_actual.get_nombre()}")
-        
+                    print(f"{num + 1} {curso_actual.nombre}")
+
                 validacion = True
                 while validacion:
                     op = input("Ingrese el número del curso que desea ver: ")
@@ -44,16 +41,14 @@ def submenu(profesor_actual):
                 curso_seleccionado = profesor_actual.mis_cursos[op - 1]
 
                 # Muestra el nombre del curso y la contraseña de matriculación
-                print(f"Nombre: {curso_seleccionado.get_nombre()}")
-                print(f"Contraseña de matriculación: {curso_seleccionado.get_contrasenia_matriculacion()}")
+                print(f"Nombre: {curso_seleccionado.nombre}")
+                print(f"Contraseña de matriculación: {curso_seleccionado.contrasenia_matriculacion}")
 
-
-
-        
         elif op == "3":
-            #volver al menú principal
+            # Volver al menú principal
             break
         else:
             print("Opción no válida")
+
 
         
