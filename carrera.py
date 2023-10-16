269d0fb9239115217238d1bfdb1eb850469800a8
facundolgomez <facundolgomez87@gmail.com>
2023-10-16 01:47:53 -0300

    creando una archivo que se usara mas adelante

---
+++ carrera.py
@@ -0,0 +1,16 @@
+#modulo de carreras, ya que el enunciado dice que el sistema cuenta con una carrera dada de alta
+
+class Carrera:
+	def __init__(self, nombre: str, cant_anios: int):
+		self.__nombre = nombre
+		self.__cant_anios = cant_anios
+
+	def __str__(self):
+		pass
+
+	
+	def get_cantidad_materias(self) -> int:
+	    pass
+
+
+	    		
