d0f8d3775ae3c171c9b1912d151623329354bef3
facundolgomez <facundolgomez87@gmail.com>
2023-10-18 15:39:03 -0300

    poniendo atr protegidos

--- usuario.py
+++ usuario.py
@@ -1,26 +1,37 @@
 from abc import ABC, abstractmethod
 
-#Esta es la clase abstracta, clase padre de Profesor y Estudiante
 class Usuario(ABC):
     conj_emails = set()
 
     def __init__(self, nombre: str, apellido: str, email: str, contrasenia: str):
-        self.nombre = nombre
-        self.apellido = apellido
+        self._nombre = nombre  
+        self._apellido = apellido  
 
-        #Aca cuando se crea una instancia se valida que el mail sea unico en base a un conjunto ya que no se puede repetir elementos
         if email in Usuario.conj_emails:
             raise ValueError("El email ya existe, debe proporcionar otro")
         else:
-            self.email = email
+            self._email = email  
             Usuario.conj_emails.add(email)
 
-        self.contrasenia = contrasenia
+        self._contrasenia = contrasenia 
 
-    def __str__(self):
-        pass
+    @property
+    def nombre(self):
+        return self._nombre
 
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def contrasenia(self):
+        return self._contrasenia
 
     @abstractmethod
     def validar_credenciales(self, email: str, contrasenia: str) -> bool:
         pass
+
