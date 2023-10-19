3e8b2b3f8adf86145dad5e5c4ccf21f62fa08a33
facundolgomez <facundolgomez87@gmail.com>
2023-10-19 00:52:48 -0300

    arreglando el menu

--- app.py
+++ app.py
@@ -1,43 +1,6 @@
-import estudiante
 import submenualumno
-import profesor
 import submenuprofesor
-
-#funciones de busqueda
-def buscando_profesor(email):
-    for profe in lista_profes_registrados:
-        if profe.email == email:
-            return profe
-    return None
-
-
-def buscando_estudiante(email):
-    for estudiante in lista_alum_registrado:
-        if estudiante.email == email:
-            return estudiante
-    return None
-
-
-
-lista_profes_registrados = []
-
-lista_alum_registrado = []
-
-lista_cursos_campus = []
-
-#profe de prueba
-profe = profesor.Profesor("Carlos", "rodriguez", "mailprofe", "contra", "ingeniero", 1998)
-lista_profes_registrados.append(profe)
-
-
-#alumnos de prueba
-estudiante1 = estudiante.Estudiante("franco", "gonzalez", "mail", "contra", 434312, 2022)
-lista_alum_registrado.append(estudiante1)
-
-estudiante2 = estudiante.Estudiante("lucas", "diaz", "otromail", "contra", 75632, 2022)
-lista_alum_registrado.append(estudiante2)
-
-
+import procesos
 
 ejecutar_programa = True 
 
@@ -54,12 +17,14 @@ while ejecutar_programa:
     if op == "1":
         gmail = input("Ingrese su correo electrónico: ")
         password = input("Ingrese su contraseña: ")
-        un_estudiante = buscando_estudiante(gmail)
+        un_estudiante = procesos.buscando_estudiante(gmail)
 
         if un_estudiante:
-            if un_estudiante.validar_credenciales(gmail, password):
-                print("ACCESO EXITOSO")
-                submenualumno.submenu(un_estudiante)
+            for un_estudiante in procesos.lista_alum_registrado:
+                if un_estudiante.validar_credenciales(gmail, password):
+                    print("ACCESO EXITOSO")
+                    submenualumno.submenu(un_estudiante)
+                    break
             else:
                 print("Correo electronico o contraseña incorrecta")
         else:
@@ -69,12 +34,14 @@ while ejecutar_programa:
     elif op == "2":
         gmail = input("Ingrese su correo electrónico: ")
         password = input("Ingrese su contraseña: ")
-        un_profesor = buscando_profesor(gmail)
+        un_profesor = procesos.buscando_profesor(gmail)
 
         if un_profesor:
-            if un_profesor.validar_credenciales(gmail, password):
-                print("ACCESO EXITOSO")
-                submenuprofesor.submenu(un_profesor)
+            for un_profesor in procesos.lista_profes_registrados:
+                if un_profesor.validar_credenciales(gmail, password):
+                    print("ACCESO EXITOSO")
+                    submenuprofesor.submenu(un_profesor)
+                    break
             else:
                 print("Correo electronico o contraseña incorrecta ")
         else:
@@ -82,8 +49,8 @@ while ejecutar_programa:
 
     # Lista de cursos del campus virtual    
     elif op == "3":
-        if lista_cursos_campus:
-            for curso in sorted(lista_cursos_campus, key=lambda curso: curso.nombre):
+        if procesos.lista_cursos_campus:
+            for curso in sorted(procesos.lista_cursos_campus, key=lambda curso: curso.nombre):
                 print(f"Materia: {curso.nombre}, Carrera: Tecnicatura Universitaria en Programación")
         else:
             print("Todavia no hay cursos disponibles en el campus virtual")
@@ -91,6 +58,7 @@ while ejecutar_programa:
 
     elif op == "4":
         ejecutar_programa = False
+
 
 
 
