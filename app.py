4e78c3b56209af3800ea2635035c1cb4c691c675
facundolgomez <facundolgomez87@gmail.com>
2023-10-20 16:45:34 -0300

    arreglando menu

--- app.py
+++ app.py
@@ -1,4 +1,81 @@
+import submenualumno
+import submenuprofesor
+import procesos
 
+
+ejecutar_programa = True 
+
+while ejecutar_programa:
+    print("Bienvenido al Campus Virtual")
+    print("<--------------------------->")
+    print("1 -> Ingresar como alumno")
+    print("2 -> Ingresar como profesor")
+    print("3 -> Ver cursos")
+    print("4 -> Salir")
+
+    op = input("Seleccione una opción: ")
+
+    # Ingresar como alumno
+    if op == "1":
+        gmail = input("Ingrese su correo electrónico: ")
+        password = input("Ingrese su contraseña: ")
+        un_estudiante = procesos.buscando_estudiante(gmail)
+
+        if un_estudiante:
+            for un_estudiante in procesos.lista_alum_registrado:
+                if un_estudiante.validar_credenciales(gmail, password):
+                    print("ACCESO EXITOSO")
+                    print("")
+                    submenualumno.submenu(un_estudiante)
+                    break
+            else:
+                print("Correo electronico o contraseña incorrecta")
+                print("")
+        else:
+            print("El estudiante no existe. Debe darse de alta en alumnado.")
+            print("")
+
+    # Ingresar como profesor            
+    elif op == "2":
+        gmail = input("Ingrese su correo electrónico: ")
+        password = input("Ingrese su contraseña: ")
+        un_profesor = procesos.buscando_profesor(gmail)
+
+        if un_profesor:
+            for un_profesor in procesos.lista_profes_registrados:
+                if un_profesor.validar_credenciales(gmail, password):
+                    print("ACCESO EXITOSO")
+                    print("")
+                    submenuprofesor.submenu(un_profesor)
+                    break
+            else:
+                print("Correo electronico o contraseña incorrecta.")
+                print("")
+        else:
+            print("El profesor no existe. Debe darse de alta en alumnado.")
+            print("")
+
+    # Lista de cursos del campus virtual    
+    elif op == "3":
+        if procesos.lista_cursos_campus:
+            longitud = max(len(curso.nombre) for curso in procesos.lista_cursos_campus)
+            for curso in sorted(procesos.lista_cursos_campus, key=lambda curso: curso.nombre):
+                # Ajusta los espacios para que el texto se muestre justificado
+                nombre_curso = curso.nombre.ljust(longitud)
+                print(f"Materia: {nombre_curso} Carrera: Tecnicatura Universitaria en Programación")
+            print("")
+
+        else:
+            print("Todavia no hay cursos disponibles en el campus virtual")
+            print("")
+
+    elif op == "4":
+        print("¡Hasta luego!")
+        ejecutar_programa = False
+
+    else:
+        print("Ingrese un número válido.")
+        print("")
 
 
 
